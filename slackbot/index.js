9429523b523506edcfc7f677fde50b67ce89ef38
김동주 <dongju.k@navercorp.com>
2023-01-07 20:41:42 +0900

    슬랙봇 기본 연동 구현

--- slackbot/index.js
+++ slackbot/index.js
@@ -1,8 +1,19 @@
-const express = require('express');
-const app = express();
+const { App } = require('@slack/bolt');
+const figlet = require('figlet');
+require('dotenv').config()
 
-app.get('/', (request, response) => {
-    response.send('asd');
+const app = new App({
+  token: process.env.BOT_TOKEN,
+  appToken: process.env.SLACK_APP_TOKEN,
+  socketMode: true,
 });
 
-app.listen(3000);
+app.message('!hello', async ({ message, say }) => {
+	say('안녕 나는 야탑봇이야~!');
+});
+
+(async () => {
+	figlet('YATAP BOT', (e, data) => console.log(data));
+  await app.start();
+  console.log('⚡️ Bolt app started');
+})();
