6eaf383f114f5314417e84e56ce78256965cd2ab
minseok <minseok9128@gmail.com>
2023-01-07 18:34:09 +0900

    app.js 수정

--- slackbot/app.js
+++ slackbot/app.js
@@ -1,41 +1,38 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const { App } = require('@slack/bolt');
+require('dotenv').config()
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+const app = new App({
+  token: process.env.BOT_TOKEN,
+  appToken: process.env.SLACK_APP_TOKEN,
+  socketMode: true,
+});
+// app.message('*', async ({ message, say }) => {
+//   await say(`Hey there <@${message.user}>!`);
+// });
+app.message('hello', async ({ message, say }) => {
+  await say(`Hey there <@${message.user}>!`);
+});
+app.command('/hello', async ({ message, say }) => {
+  await say(`@@@Hey there <@>!`);
+});
+app.message('5번서버', async ({ message, say }) => {
+  await say(`5번서버 접속정보 `);
+});
+app.message('문화정보원 엑셀', async ({ message, say }) => {
+  await say(`5번서버 접속정보 
+            
+            pw : dasdas
+            `);
+});
+//
+app.message(/(.+)접속정보/, async ({ message, say }) => {
+  await say(`5번서버 접속정보 :
+            
+            pw : dasdas`);
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
+(async () => {
+  await app.start();
+  console.log('⚡️ Bolt app started');
+})();
